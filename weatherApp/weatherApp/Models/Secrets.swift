9580fa9286e2569771d2acf449b64b703c2cabd2
Sam <samroman3@gmail.com>
2019-10-10 16:40:41 -0400

    Adds Secrets file

---
+++ weatherApp/weatherApp/Models/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  weatherApp
+//
+//  Created by Sam Roman on 10/10/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import Foundation
+
+
+struct Secrets {
+    static var api_key = "fc6747fc3f155dcec711ad5f69cea503"
+}
---
+++ weatherApp/weatherApp/Models/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  weatherApp
+//
+//  Created by Sam Roman on 10/10/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import Foundation
