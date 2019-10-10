a3e3e409829fd8ac9ebe8f5a0dd303a66b8d0160
Sam <samroman3@gmail.com>
2019-10-10 16:47:36 -0400

    Adds model to parse Weather from DarkSky API

--- weatherApp/weatherApp/Models/Weather.swift
+++ weatherApp/weatherApp/Models/Weather.swift
@@ -7,3 +7,116 @@
 //
 
 import Foundation
+
+// MARK: - Weather
+struct Weather: Codable {
+    let latitude, longitude: Double
+    let timezone: String
+    let currently: Currently
+    let minutely: Minutely
+    let hourly: Hourly
+    let daily: Daily
+    let flags: Flags
+    let offset: Int
+}
+
+// MARK: - Currently
+struct Currently: Codable {
+    let time: Int
+    let summary: Summary
+    let icon: Icon
+    let nearestStormDistance, nearestStormBearing: Int?
+    let precipIntensity, precipProbability, temperature, apparentTemperature: Double
+    let dewPoint, humidity, pressure, windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility, ozone: Double
+    let precipType: String?
+}
+
+enum Icon: String, Codable {
+    case clearDay = "clear-day"
+    case clearNight = "clear-night"
+    case partlyCloudyDay = "partly-cloudy-day"
+    case partlyCloudyNight = "partly-cloudy-night"
+}
+
+enum Summary: String, Codable {
+    case clear = "Clear"
+    case mostlyCloudy = "Mostly Cloudy"
+    case partlyCloudy = "Partly Cloudy"
+}
+
+// MARK: - Daily
+struct Daily: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [DailyDatum]
+}
+
+// MARK: - DailyDatum
+struct DailyDatum: Codable {
+    let time: Int
+    let summary: String
+    let icon: Icon
+    let sunriseTime, sunsetTime: Int
+    let moonPhase, precipIntensity, precipIntensityMax: Double
+    let precipIntensityMaxTime: Int
+    let precipProbability: Double
+    let precipType: String
+    let temperatureHigh: Double
+    let temperatureHighTime: Int
+    let temperatureLow: Double
+    let temperatureLowTime: Int
+    let apparentTemperatureHigh: Double
+    let apparentTemperatureHighTime: Int
+    let apparentTemperatureLow: Double
+    let apparentTemperatureLowTime: Int
+    let dewPoint, humidity, pressure, windSpeed: Double
+    let windGust: Double
+    let windGustTime, windBearing: Int
+    let cloudCover: Double
+    let uvIndex, uvIndexTime: Int
+    let visibility, ozone, temperatureMin: Double
+    let temperatureMinTime: Int
+    let temperatureMax: Double
+    let temperatureMaxTime: Int
+    let apparentTemperatureMin: Double
+    let apparentTemperatureMinTime: Int
+    let apparentTemperatureMax: Double
+    let apparentTemperatureMaxTime: Int
+}
+
+// MARK: - Flags
+struct Flags: Codable {
+    let sources: [String]
+    let nearestStation: Double
+    let units: String
+
+    enum CodingKeys: String, CodingKey {
+        case sources
+        case nearestStation = "nearest-station"
+        case units
+    }
+}
+
+// MARK: - Hourly
+struct Hourly: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [Currently]
+}
+
+// MARK: - Minutely
+struct Minutely: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [MinutelyDatum]
+}
+
+// MARK: - MinutelyDatum
+struct MinutelyDatum: Codable {
+    let time, precipIntensity, precipProbability: Int
+}
